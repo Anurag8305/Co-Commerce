a4fdfb694542daeb26628e5caaf4f8e86c68241e
Anurag Raj <anuragraj8305@gmail.com>
2023-01-18 12:44:05 +0530

    Routing

--- limeroadclone/src/Routes/AllRoutes.jsx
+++ limeroadclone/src/Routes/AllRoutes.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import {Routes,Route} from "react-router-dom";
+import CartPage from "../Pages/CartPage";
+import HomePage from "../Pages/HomePage";
+import LandingPage from "../Pages/LandingPage";
+import Login from "../Pages/Login";
+import Mens from "../Pages/Mens";
+import Payment from "../Pages/Payment";
+import Women from "../Pages/Women";
 
 const AllRoutes = () => {
-	return <div>AllRoutes</div>;
+	return <div>
+		<Routes>
+			<Route path="/" element={<LandingPage /> } />
+			<Route path="/men" element={<Mens /> } />
+			<Route path="/women" element={<Women /> } />
+			<Route path="/homepage" element={<HomePage /> } />
+			<Route path="/login" element={<Login /> } />
+			<Route path="/cart" element={<CartPage /> } />
+			<Route path="/payment" element={<Payment /> } />
+		</Routes>
+	</div>;
 };
 
 export default AllRoutes;
