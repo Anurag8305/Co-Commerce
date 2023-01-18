0164e1a8981c9c43abb5c43370e9aef1168b62d3
Anurag Raj <anuragraj8305@gmail.com>
2023-01-18 12:56:24 +0530

    Routing

--- limeroadclone/src/Pages/HomePage.jsx
+++ limeroadclone/src/Pages/HomePage.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+	return (
+		<div>
+			<h1>HomePage</h1>
+		</div>
+	);
+};
 
-export default HomePage
+export default HomePage;
